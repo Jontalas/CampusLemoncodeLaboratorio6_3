aa7b56a3b27cf25784333a703a8501a4931af875
Jonatan <jontalas@gmail.com>
2021-12-30 01:53:25 +0100

    Inicio

---
+++ Laboratorio6_3.js
@@ -0,0 +1,31 @@
+// Constantes
+var WORK_HOURS = [
+    "08:00 - 09:00",
+    "09:00 - 10:00",
+    "10:00 - 11:00",
+    "11:00 - 12:00",
+    "12:00 - 13:00",
+    "13:00 - 14:00",
+    "15:00 - 16:00",
+    "16:00 - 17:00"
+  ];
+  
+  // Datos
+  var myTeam = [
+    {
+      name: "María",
+      availability: new Array(8).fill(true)
+    },
+    {
+      name: "Pedro",
+      availability: new Array(8).fill(true)
+    },
+    {
+      name: "Esther",
+      availability: new Array(8).fill(true)
+    },
+    {
+      name: "Marcos",
+      availability: new Array(8).fill(true)
+    },
+  ];
