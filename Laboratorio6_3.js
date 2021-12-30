fce7dc48bc495df02db7e249e9b1a57e4c1bb344
Jonatan <jontalas@gmail.com>
2021-12-30 03:01:30 +0100

    disponibilidad de equipo terminado

--- Laboratorio6_3.js
+++ Laboratorio6_3.js
@@ -1,3 +1,5 @@
+// Disponibilidad de equipo -------------------------
+
 // Constantes
 var WORK_HOURS = [
     "08:00 - 09:00",
@@ -8,24 +10,64 @@ var WORK_HOURS = [
     "13:00 - 14:00",
     "15:00 - 16:00",
     "16:00 - 17:00"
-  ];
-  
-  // Datos
-  var myTeam = [
+];
+
+// Datos
+var myTeam = [
     {
-      name: "María",
-      availability: new Array(8).fill(true)
+        name: "María",
+        availability: new Array(8).fill(true)
     },
     {
-      name: "Pedro",
-      availability: new Array(8).fill(true)
+        name: "Pedro",
+        availability: new Array(8).fill(true)
     },
     {
-      name: "Esther",
-      availability: new Array(8).fill(true)
+        name: "Esther",
+        availability: new Array(8).fill(true)
     },
     {
-      name: "Marcos",
-      availability: new Array(8).fill(true)
+        name: "Marcos",
+        availability: new Array(8).fill(true)
     },
-  ];
+];
+
+var randomInit = () => {
+    for (const valor of myTeam) {
+        var schedule = valor.availability;
+        for (const key in schedule) {
+            schedule[key] = (Math.round(Math.random()) ? true : false);
+        }
+    }
+}
+
+var showSchedule = () => {
+    for (const valor of myTeam) {
+        console.log("Disponibilidad de " + valor.name)
+        var schedule = valor.availability;
+        for (const key in schedule) {
+            console.log("    " + WORK_HOURS[key] + ": " + (schedule[key] ? "Si" : "No"));
+        }
+    }
+}
+
+var lookForMeeting = () => {
+    var meetingFound = false;
+    var workHoursIter = 0;
+    do {
+        var myTeamIter = 0;
+        do {
+            meetingFound = myTeam[myTeamIter].availability[workHoursIter];
+        } while (meetingFound && ++myTeamIter < myTeam.length);
+    } while (!meetingFound && ++workHoursIter < WORK_HOURS.length);
+    if (meetingFound) {
+        console.log("Hueco encontrado en el horario " + WORK_HOURS[workHoursIter]);
+    }
+    else {
+        console.log("Lo siento. No hay hueco disponible en el equipo.");
+    }
+}
+// MAIN -------------------------
+randomInit();
+showSchedule();
+lookForMeeting();
